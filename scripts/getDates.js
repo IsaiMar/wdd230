8c079d5456af8d1411e01424dd18cabebae5d73d
IsaiMar <80867603+IsaiMar@users.noreply.github.com>
2023-04-27 08:54:16 -0600

    final

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -2,5 +2,5 @@
 let year = new Date().getFullYear()
 document.getElementById('year').innerHTML = "&copy;" + year + "  Isai Mar Mexico" ;
 
-let oLastModif = new Date(document.lastModified);
-document.getElementById('lastModified').innerHTML = "Last Modification:" + oLastModif;
+let oLastModif = document.lastModified;
+document.getElementById('lastModified').innerHTML = "Last Modification: " + oLastModif;
