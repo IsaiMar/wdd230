b251c686fe941018f7c47c97f1c3615e93b3768e
IsaiMar <80867603+IsaiMar@users.noreply.github.com>
2024-05-02 11:09:04 -0600

    added images and new chamber site

---
+++ scripts/getDates.js
@@ -0,0 +1,6 @@
+
+let year = new Date().getFullYear()
+document.getElementById('year').innerHTML = "&copy;" + year + "  Isai Mar, Mexico" ;
+
+let LastModif = document.lastModified;
+document.getElementById('lastModified').innerHTML = "Last Modification: " + LastModif;
